25c4a8bdafaaaa63c7d22bfeef8f09cace2dc980
kavyan2004 <s190049@rguktsklm.ac.in>
2024-04-30 11:14:07 +0530

    manually created the tables in the dp by removind ddl-auto command

---
+++ registration-login/src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+create database loginSystem;
+
+use loginSystem;
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(50) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+   PRIMARY KEY (`role_id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `person` (
+  `person_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile_number` varchar(20) NOT NULL,
+  `pwd` varchar(200) NOT NULL,
+  `role_id` int NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+   PRIMARY KEY (`person_id`),
+   FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
